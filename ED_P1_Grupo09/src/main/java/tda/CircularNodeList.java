c9f54ff456e48110139b7941ce5a35903b3b8f89
Andrés Bohórquez <andbvill@espol.edu.ec>
2024-06-23 17:31:27 -0500

    implementacion

--- ED_P1_Grupo09/src/main/java/tda/CircularNodeList.java
+++ ED_P1_Grupo09/src/main/java/tda/CircularNodeList.java
@@ -12,25 +12,36 @@ package tda;
 public class CircularNodeList<E> {
     private E content;
     private CircularNodeList<E> next;
+    private CircularNodeList<E> previous;
     
     public CircularNodeList(E content){
-        this.content=content;
-        this.next=this; // Instancia de un nodo el next ya no apunta a null sino a si mismo
+        this.content = content;
+        this.next = this; // Instancia de un nodo el next ya no apunta a null sino a si mismo
+        this.previous = this;  //Apunta a si mismo igualmente al ser circular.
     }
     
-    public E getContent(){
+   public E getContent() {
         return content;
     }
-    
-    public void setContent(E content){
+
+    public void setContent(E content) {
         this.content = content;
     }
-    
-    public CircularNodeList<E> getNext(){
+
+    public CircularNodeList<E> getNext() {
         return next;
     }
-    
-    public void setNext(CircularNodeList<E> next){
+
+    public void setNext(CircularNodeList<E> next) {
         this.next = next;
     }
+
+    public CircularNodeList<E> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(CircularNodeList<E> previous) {
+        this.previous = previous;
+    }
 }
+
